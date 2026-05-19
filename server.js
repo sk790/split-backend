146be0387fd0153cf69caa1aae462c3f8462e65b
Saurabh Nagar <saurabhk2890@gmail.com>
2026-05-19 20:13:29 +0530

    Revert "feat: increase JSON and URL-encoded payload limits and remove debug logging from expense controller"
    This reverts commit 7c77ebcc2465c4fd021e730c9cfe74f50c244448.

--- server.js
+++ server.js
@@ -20,8 +20,7 @@ connectDB();
 
 // Middleware
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
--- src/controllers/expenseController.js
+++ src/controllers/expenseController.js
@@ -7,6 +7,8 @@ const { calculateBalances } = require("../utils/balanceCalculator");
 exports.addExpense = async (req, res) => {
   try {
     const { amount, splitBetween, description, paidBy, category, attachment } = req.body;
+    console.log(category,'ca');
+    
     const groupId = req.params.id;
     const paidById = paidBy || req.user._id;
 
